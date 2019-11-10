ab2f0ee28d3065c501cadc895ea035b86d62d6a0
AxmetES <axmet-1984@mail.ru>
2019-11-10 22:25:04 +0600

    wait for response

--- devman_bot.py
+++ devman_bot.py
@@ -7,7 +7,13 @@ from const import *
 bot = telegram.Bot(token=TOKEN)
 
 # bot.send_message(chat_id=421935954, text=' Извини, Дэйв, боюсь, я не могу этого сделать')
+
+
+response = requests.get(dev_long_URL, headers=headers)
+pprint.pprint(response.json())
+
 while True:
     response = requests.get(dev_long_URL, headers=headers)
     for message in response:
-        bot.send_message(chat_id=421935954, text=response.json())
+        if {"status": "found"} in response:
+            bot.send_message(chat_id=421935954, text=response.json())
