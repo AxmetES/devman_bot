2a476dfc64d822eb8621be039cd78b1f8d556969
AxmetES <axmet-1984@mail.ru>
2019-11-13 23:02:08 +0600

    split bot message to negative and positive message

--- devman_bot.py
+++ devman_bot.py
@@ -8,38 +8,39 @@ from const import *
 pp = telegram.utils.request.Request(proxy_url='socks5h://95.141.130.122:9999')
 bot = telegram.Bot(token=TOKEN, request=pp)
 
-bot.send_message(chat_id=421935954, text=' Извини, Дэйв, боюсь, я не могу этого сделать')
+timestamp = {}
 
-# requesr-respons function
-# timestamp = {'timestamp_to_request': 0}
+while True:
+    try:
+        response = requests.get(dev_long_URL, headers=headers, params=timestamp)
+        dict_resp = response.json()
+        if dict_resp.get('status') == 'found':
+            last_timestamp = dict_resp.get('last_attempt_timestamp')
+            timestamp = {'timestamp_to_request': {last_timestamp}}
+            answer = dict_resp.get('new_attempts')
+            if answer.get('is_negative') == True:
+                teach_str = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
+            else:
+                teach_str = 'к сожелению в работе нашлись ошибки'
+            bot.send_message(chat_id=421935954, text=f'Преподаватель проверил работу! {teach_str}')
+        else:
+            last_timestamp = dict_resp.get('timestamp_to_request')
+            timestamp = {'timestamp_to_request': {last_timestamp}}
+    except ConnectionError:
+        continue
+
+# else:
+#     try:
+#         response = requests.get(dev_long_URL, headers=headers, params=timestamp)
+#         dict_resp = response.json()
 #
-# while True:
-#     if timestamp.get('timestamp_to_request') == 0:
-#         try:
-#             response = requests.get(dev_long_URL, headers=headers)
-#             dict_resp = response.json()
-#
-#             if dict_resp.get('status') == 'found':
-#                 timestamp['timestamp_to_request'] = dict_resp.get('last_attempt_timestamp')
-#                 pprint.pprint(dict_resp)
-#                 print(timestamp)
-#             else:
-#                 timestamp['timestamp_to_request'] = dict_resp.get('timestamp_to_request')
-#         except ConnectionError:
-#             continue
-#     else:
-#         try:
-#             response = requests.get(dev_long_URL, headers=headers, params=timestamp)
-#             dict_resp = response.json()
-#
-#             if dict_resp.get('status') == 'found':
-#                 timestamp['timestamp_to_request'] = dict_resp.get('last_attempt_timestamp')
-#                 pprint.pprint(dict_resp)
-#                 print(timestamp)
-#             else:
-#                 timestamp['timestamp_to_request'] = dict_resp.get('timestamp_to_request')
-#         except ConnectionError:
-#             continue
+#         if dict_resp.get('status') == 'found':
+#             timestamp['timestamp_to_request'] = dict_resp.get('last_attempt_timestamp')
+#             bot.send_message(chat_id=421935954, text='Преподаватель проверил работу!')
+#         else:
+#             timestamp['timestamp_to_request'] = dict_resp.get('timestamp_to_request')
+#     except ConnectionError:
+#         continue
 
 # respons  - example
 # {'last_attempt_timestamp': 1573572410.077114,
--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 timestamp = {'timestamp_to_request': 0}
 dict_resp = {'last_attempt_timestamp': 1573572410.077114,
-             'new_attempts': [{'is_negative': True,
+             'new_attempts': [{'is_negative': False,
                                'lesson_title': 'Отправляем уведомления о проверке работ',
                                'lesson_url': '/modules/chat-bots/lesson/devman-bot/',
                                'submitted_at': '2019-11-12T18:26:50.077114+03:00',
@@ -8,6 +8,7 @@ dict_resp = {'last_attempt_timestamp': 1573572410.077114,
              'request_query': [],
              'status': 'found'}
 
-
-
-print(timestamp)
+answer_list = dict_resp.get('new_attempts')[0]
+# dic_attempts = answer_list[0]
+if answer_list.get('is_negative') == False:
+    print('test in')
