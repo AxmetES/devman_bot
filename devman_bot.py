c52eabd0db693275b2aac25482b018e92038d70f
AxmetES <axmet-1984@mail.ru>
2019-12-23 17:55:16 +0600

    dev_bot lesson 1 , improvements 6

--- devman_bot.py
+++ devman_bot.py
@@ -1,33 +1,50 @@
-import logging
 import requests
 import telegram
 import os
 from const import headers, dev_long_URL
 from proxy_broker import get_proxy
 
-URL_DEVMAN_LESSON = 'https://dvmn.org/modules/'
-
 
 def get_time_stump(json):
-    if 'found' in json:
+    if json['status'] == 'found':
         element_of_response = json.get('last_attempt_timestamp')
         request_params = {'timestamp_to_request': {f'{element_of_response}'}}
+        print(request_params)
         return request_params
     else:
         element_of_response = json.get('timestamp_to_request')
         request_params = {'timestamp_to_request': {f'{element_of_response}'}}
+        print(request_params)
         return request_params
 
 
-def get_message(json):
-    if not json['new_attempts'][0]['is_negative']:
-        message = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
-    else:
-        message = 'К сожалению в работе нашлись ошибки'
+def get_message(json, chat_id, bot):
+    url_devman = 'https://dvmn.org'
+    message = ''
 
-    full_message = f'''У вас проверили работу, отправляем уведомление о проверке работ,
-                        {message}  Ссылка на урок: {URL_DEVMAN_LESSON}'''
-    return full_message
+    if json['status'] == 'timeout':
+        pass
+    elif json['status'] == 'found':
+        if not json['new_attempts'][0]['is_negative']:
+            message = 'Преподавателю все понравилось, можно приступать к следующему уроку!'
+        elif json['new_attempts'][0]['is_negative']:
+            message = 'К сожалению в работе нашлись ошибки'
+        url_lesson = json['new_attempts'][0]['lesson_url']
+
+        url_devman = url_devman + url_lesson
+        lesson_title = json['new_attempts'][0]['lesson_title']
+        full_message = f'''{lesson_title}, {message}  Ссылка на урок: {url_devman}'''
+        bot.send_message(chat_id=chat_id, text=full_message)
+
+
+def get_request(url, headers, params):
+    response = requests.get(url=url, headers=headers, params=params)
+    response.raise_for_status()
+    dict_resp = response.json()
+
+    if 'error' in response.text:
+        raise requests.exceptions.HTTPError(dict_resp['error'])
+    return dict_resp
 
 
 def main():
@@ -40,17 +57,11 @@ def main():
     request_params = {}
     while True:
         try:
-            response = requests.get(dev_long_URL, headers=headers, params=request_params)
-            response.raise_for_status()
-            dict_resp = response.json()
-            if 'error' in dict_resp:
-                raise requests.exceptions.HTTPError(dict_resp['error'])
+            dict_resp = get_request(dev_long_URL, headers, request_params)
 
             request_params = get_time_stump(dict_resp)
 
-            user_message = get_message(dict_resp)
-
-            bot.send_message(chat_id=chat_id, text=user_message)
+            get_message(dict_resp, chat_id, bot)
 
         except requests.exceptions.ConnectionError as error:
             print(f'{error}')
