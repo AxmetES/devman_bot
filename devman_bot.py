2712d785de6bcb40d6e846982bace2c60f7dfdec
AxmetES <frakstia@gmail.com>
2020-02-11 14:45:18 +0600

    -Changed get_logger func. -Logger description relocate in def main func. - In def emit(self, record): - Add self.chat_id parametre. - In func get_time_stamp changed parametre request_parametr from str to fload.

--- devman_bot.py
+++ devman_bot.py
@@ -5,18 +5,19 @@ import telegram
 from proxy_broker import get_proxy
 import os
 import logging
+from dotenv import load_dotenv
 
-chat_id = os.getenv('CHAT_ID')
+logger = logging.getLogger('bot logger')
 
 
 def get_time_stamp(response):
     if response['status'] == 'found':
         timestamp = response.get('last_attempt_timestamp')
-        request_params = {'timestamp_to_request': f'{timestamp}'}
+        request_params = {'timestamp_to_request': timestamp}
         return request_params
     else:
         timestamp = response.get('timestamp_to_request')
-        request_params = {'timestamp_to_request': f'{timestamp}'}
+        request_params = {'timestamp_to_request': timestamp}
         return request_params
 
 
@@ -48,35 +49,33 @@ def request_devman_api(url, headers, params):
     return response_data
 
 
-def get_logs(bot):
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger('bot logger')
-    logger.setLevel(logging.INFO)
-    handler = BotLoggerHandler(bot=bot)
-    logger.addHandler(handler)
-    return logger
-
-
 def main():
-    dev_long_url = 'https://dvmn.org/api/long_polling/'
+    load_dotenv(verbose=True)
+    long_polling_url = 'https://dvmn.org/api/long_polling/'
     devman_token = os.getenv('DEVMAN_TOKEN')
     headers = {'Authorization': f'Token {devman_token}'}
 
     proxy = get_proxy()
     pp = telegram.utils.request.Request(proxy_url=proxy)
     bot = telegram.Bot(token=os.getenv('BOT_TOKEN'), request=pp)
+    chat_id = os.getenv('CHAT_ID')
 
-    logger = get_logs(bot)
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    logger.setLevel(logging.INFO)
+    handler = BotLoggerHandler(bot=bot, chat_id=chat_id)
+    logger.addHandler(handler)
+
     logger.info('bot is started')
     request_params = {}
     while True:
         try:
-            response = request_devman_api(dev_long_url, headers, request_params)
+            response = request_devman_api(long_polling_url, headers, request_params)
             request_params = get_time_stamp(response)
             send_message(response, chat_id, bot)
         except requests.exceptions.ConnectionError as error:
             logger.debug(f'{error}')
+            time.sleep(30)
         except requests.exceptions.Timeout as error:
             logger.debug(f'{error}')
         except requests.exceptions.HTTPError as error:
@@ -87,13 +86,14 @@ def main():
 
 
 class BotLoggerHandler(logging.Handler):
-    def __init__(self, bot):
+    def __init__(self, bot, chat_id):
         super().__init__()
         self.bot = bot
+        self.chat_id = chat_id
 
     def emit(self, record):
         log_entry = self.format(record)
-        self.bot.send_message(chat_id=chat_id, text=log_entry)
+        self.bot.send_message(self.chat_id, text=log_entry)
 
 
 if __name__ == '__main__':
